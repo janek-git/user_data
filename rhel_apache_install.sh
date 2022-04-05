70ba9ec8686153e21cd05b488ac336fb6d5961e1
Janek <janek.wenzel@gmail.com>
2022-04-05 20:43:32 +0200

    hgj

--- rhel_apache_install.sh
+++ rhel_apache_install.sh
@@ -1,5 +1,14 @@
-yum install -y httpd.x86_64
-systemctl start httpd.service
-systemctl enable httpd.service
+#!/bin/bash
+yum update -y
+amazon-linux-extras install -y lamp-mariadb10.2-php7.2 php7.2
+yum install -y httpd mariadb-server
+systemctl start httpd
+systemctl enable httpd
 yum install -y mod_ssl
-systemctl restart httpd.service
+systemctl restart httpd
+usermod -a -G apache ec2-user
+chown -R ec2-user:apache /var/www
+chmod 2775 /var/www
+find /var/www -type d -exec chmod 2775 {} \;
+find /var/www -type f -exec chmod 0664 {} \;
+echo "<?php phpinfo(); ?>" > /var/www/html/phpinfo.php
